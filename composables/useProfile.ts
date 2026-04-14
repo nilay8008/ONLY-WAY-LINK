75b1bfa7271bfbcd4445cb0413cb8503d1208e0e
nilay8008 <nilaypatel309@gmail.com>
2026-04-14 16:00:31 +0530

    CodeBase Update

--- composables/useProfile.ts
+++ composables/useProfile.ts
@@ -1,18 +1,14 @@
 export const useProfile = () => {
   const profile = {
     name: 'NILAY PATEL',
-    bio: "I'm Nilay | Mapping on chain alpha & decoding Web3 systems ⚡",
-    avatar: '../assets/images/ni.jpg', // replace with real avatar URL
+    bio: "I'm Nilay | Web3 & DeFi researcher focused on On chain intelligence",
+    avatar: 'https://raw.githubusercontent.com/nilay8008/MyImages/main/nip.jpeg', // replace with real avatar URL
     socials: [
-      { id: 'facebook',   icon: 'uil:facebook',   label: 'Facebook',  href: '#' },
-      { id: 'twitter',    icon: 'uil:twitter',     label: 'Twitter',   href: '#' },
-      { id: 'instagram',  icon: 'uil:instagram',   label: 'Instagram', href: '#' },
-      { id: 'telegram',   icon: 'uil:telegram',    label: 'Telegram',  href: '#' },
-      { id: 'whatsapp',   icon: 'uil:whatsapp',    label: 'WhatsApp',  href: '#' },
-      { id: 'youtube',    icon: 'uil:youtube',     label: 'YouTube',   href: '#' },
-      { id: 'email',      icon: 'uil:envelope',    label: 'Email',     href: 'mailto:john@example.com' },
-      { id: 'github',     icon: 'uil:github',      label: 'GitHub',    href: '#' },
-      { id: 'linkedin',   icon: 'uil:linkedin',    label: 'LinkedIn',  href: '#' },
+      { id: 'twitter',    icon: 'uil:twitter',     label: 'Twitter',   href: 'https://x.com/0xnilay' },
+      { id: 'telegram',   icon: 'uil:telegram',    label: 'Telegram',  href: 'https://t.me/OxNilay' },
+      { id: 'email',      icon: 'uil:envelope',    label: 'Email',     href: 'mailto:nilaypatel309@gmail.com' },
+      { id: 'github',     icon: 'uil:github',      label: 'GitHub',    href: 'https://github.com/nilay8008' },
+      { id: 'linkedin',   icon: 'uil:linkedin',    label: 'LinkedIn',  href: 'https://www.linkedin.com/in/0xnilay/' },
     ],
     links: [
       {
@@ -22,28 +18,10 @@ export const useProfile = () => {
         href: 'https://example.com',
       },
       {
-        id: 'amazon',
-        icon: 'uil:amazon',
-        label: 'Amazon wishlist',
-        href: '#',
-      },
-      {
-        id: 'react',
-        icon: 'uil:atom',
-        label: 'React JS course',
-        href: '#',
-      },
-      {
-        id: 'donate',
-        icon: 'uil:heart-medical',
-        label: 'Donate for our cause',
-        href: '#',
-      },
-      {
-        id: 'resume',
+        id: 'Resume',
         icon: 'uil:file-alt',
         label: 'Download my resume',
-        href: '#',
+        href: 'https://drive.google.com/file/d/1WFLzrFG9HUu-SvNVTAB8QmXoVqtdgiAt/view?usp=sharing',
       },
     ],
   }
